377d0fd98dee86f6c8846b93e7c605bd73d47d96
John Clarence Palma <johnclarencepalma7@gmail.com>
2025-11-04 01:44:08 +0800

    from the lecture react card project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,121 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const skillsArr = [
+  {
+    skill: "HTML",
+    color: "orangered",
+    textColor: "white",
+    level: "Intermediate",
+
+  },
+  {
+    skill: "CSS",
+    color: "blue",
+    textColor: "white",
+    level: 'Intermediate',
+  },
+  {
+    skill: "JavaScript",
+    color: "yellow",
+    textColor: "black",
+    level: "Intermediate"
+
+  },
+  {
+    skill: "React",
+    color: "skyBlue",
+    textColor: "black",
+    level: "Beginner"
+  },
+  {
+    skill: "Version Control",
+    color: "black",
+    textColor: "white",
+    level: "Intermediate"
+
+  },
+  {
+    skill: "PHP",
+    color: "#B0B3D6",
+    textColor: "white",
+    level: "Beginner"
+  },
+  {
+    skill: "Sql",
+    color: "gray",
+    textColor: "white",
+    level: "Beginner"
+  },
+  {
+    skill: "Web design",
+    color: "violet",
+    textColor: "black",
+    level: "Advance"
+  }
+];
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar name='John Dev' photoPath='pfp/sdf.jpg' />
+      <div className="data">
+        <Intro name='John' intro='Aspring Full stack webdveloper, currently learning React.' />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar({ name, photoPath }) {
+  return (
+    <header>
+      <img className='avatar' src={photoPath} alt={name}></img>
+    </header>
+  )
+}
+
+function Intro({ name, intro }) {
+  return (
+    <main>
+      <h1>{name}</h1>
+      <p>{intro}</p>
+    </main>
+  )
+}
+
+function SkillList() {
+  const ifExist = skillsArr
+  const count = ifExist.length // true
+
+
+  return (
+    <div className='skill-list' >
+      {count ? (ifExist.map((skill) => <Skill defaultVal={skill.skill} showSkill={skill} key={skill.skill} />)) : <PopMessage />}
+    </div>
+  )
+}
+
+
+function PopMessage() {
+  return (
+    <span>You don't have data!</span>
+  )
+}
+
+function Skill({ showSkill, defaultVal }) {
+
+  return (
+    <div className='skill' style={{ backgroundColor: showSkill.color, color: showSkill.textColor, fontWeight: showSkill.fw }}>
+      <span>{defaultVal}</span>
+      <span>{showSkill.level === 'Beginner' && '👶🏻'}</span>
+      <span>{showSkill.level === 'Intermediate' && '👍🏻'}</span>
+      <span>{showSkill.level === 'Advance' && '💪🏻'}</span>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<React.StrictMode><App /></React.StrictMode>)
